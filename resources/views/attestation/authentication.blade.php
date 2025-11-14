dc4b6c84f4720c7f98abd85b1adbc92d15f6e0e0
vinod-ayckattu <vinodharidas.dev24@gmail.com>
2025-11-14 12:35:51 +0530

    radio projection

--- resources/views/attestation/authentication.blade.php
+++ resources/views/attestation/authentication.blade.php
@@ -16,19 +16,19 @@
             <div class="manage-width-75 manage-width p-3 mx-2 rounded  align-items-center text-center form-scroll bg-ash ">
                 <div class="card text-start my-2">
                     <div class="card-body">
-                        <div class="border p-2 m-2 rounded">
+                        <div class="border p-2 m-2 rounded bg-ash">
                             هل تم تصديق الوثيقة مسبقًا من إحدى الجهات التالية؟<br>
                             <div class="form-group">
-                                <input type="radio" class="form-check-input" name="authentication" onclick="enableSubmit()" value="Ministry of Foreign Affairs of Yemen">
+                                <input type="radio" class="form-check-input" style="outline: 2px solid brown;" name="authentication" onclick="enableSubmit()" value="Ministry of Foreign Affairs of Yemen">
                                 <label for="authentication">وزارة الخارجية اليمنية</label>
                             </div>
                              <div class="form-group">
-                                <input type="radio" class="form-check-input" name="authentication" onclick="enableSubmit()" value="Ministry of Foreign Affairs of the United Arab Emirates
+                                <input type="radio" class="form-check-input" style="outline: 2px solid brown;" name="authentication" onclick="enableSubmit()" value="Ministry of Foreign Affairs of the United Arab Emirates
 ">
                                 <label for="authentication">وزارة الخارجية الإماراتية</label>
                             </div>
                              <div class="form-group">
-                                <input type="radio" class="form-check-input" name="authentication" onclick="enableSubmit()" value="One of the diplomatic missions of the Republic of Yemen abroad">
+                                <input type="radio" class="form-check-input" style="outline: 2px solid brown;" name="authentication" onclick="enableSubmit()" value="One of the diplomatic missions of the Republic of Yemen abroad">
                                 <label for="authentication">إحدى بعثات الجمهورية اليمنية في الخارج ( سفارة أو قنصلية عامة)</label>
                             </div>
                             <br><br>
